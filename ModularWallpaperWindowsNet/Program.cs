fe3b7fbf28441decc07b351b99e09e23e2f17719
Rolando Islas <elegantbansheea@gmail.com>
2022-06-29 18:30:47 -0700

    Initial commit

---
+++ ModularWallpaperWindowsNet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace ModularWallpaperWindowsNet
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ProcessStartInfo processStartInfo;
+            Process process;
+
+            String jarPath = Path.Combine(Environment.GetEnvironmentVariable("APPDATA"), 
+                "ModularWallpaper\\ModularWallpaperWindows.jar");
+            processStartInfo = new ProcessStartInfo("java", "-jar " + jarPath);
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.UseShellExecute = false;
+
+            process = Process.Start(processStartInfo);
+        }
+    }
+}
